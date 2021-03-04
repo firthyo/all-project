2e537448d771a95ff1dea089b315a17c405831c6
firthyo <firth.maneesuksri@gmail.com>
2021-03-04 15:41:40 +0700

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,49 +1,25 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar
       :clipped-left="clipped"
       fixed
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-icon @click="homePage">mdi-chevron-left</v-icon>
 
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-<!--      <v-text-field-->
-<!--        v-model="search"-->
-<!--        append-icon="mdi-magnify"-->
-<!--        label="Search"-->
-<!--        single-line-->
-<!--        hide-details-->
-<!--      ></v-text-field>-->
+      <v-toolbar-title v-text="title"/>
+      <v-spacer/>
+      <!--      <v-text-field-->
+      <!--        v-model="search"-->
+      <!--        append-icon="mdi-magnify"-->
+      <!--        label="Search"-->
+      <!--        single-line-->
+      <!--        hide-details-->
+      <!--      ></v-text-field>-->
     </v-app-bar>
     <v-main>
 
-        <nuxt />
+      <nuxt/>
 
     </v-main>
     <v-footer
@@ -57,7 +33,7 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       clipped: false,
       drawer: false,
@@ -78,6 +54,11 @@ export default {
       right: true,
       rightDrawer: false,
       title: 'All Project'
+    }
+  },
+  methods: {
+    homePage() {
+      this.$router.push({path: `/project/`})
     }
   }
 }
